4887a3c86c2083ff124cf442f6fb161d1ceacf45
Lidiomar Fernando dos Santos Machado <lidiomarfernandomachado@Lidiomars-Mac-mini.local>
2023-05-27 18:57:23 -0300

    Add note list layout[2]

--- AlpacaNote/Model/NotesModelData.swift
+++ AlpacaNote/Model/NotesModelData.swift
@@ -13,15 +13,15 @@ final class NotesModelData: ObservableObject {
         Note(id: UUID(), title: "Note 1", description: "Description 1"),
         Note(id: UUID(), title: "Note 2", description: "Description 2"),
         Note(id: UUID(), title: "Note 3", description: "Description 3"),
-        Note(id: UUID(), title: "Note 1", description: "Description 4"),
-        Note(id: UUID(), title: "Note 2", description: "Description 5"),
-        Note(id: UUID(), title: "Note 3", description: "Description 6"),
-        Note(id: UUID(), title: "Note 1", description: "Description 7"),
-        Note(id: UUID(), title: "Note 2", description: "Description 8"),
-        Note(id: UUID(), title: "Note 3", description: "Description 9"),
-        Note(id: UUID(), title: "Note 1", description: "Description 10"),
-        Note(id: UUID(), title: "Note 2", description: "Description 11"),
-        Note(id: UUID(), title: "Note 3", description: "Description 12")
+        Note(id: UUID(), title: "Note 4", description: "Description 4"),
+        Note(id: UUID(), title: "Note 5", description: "Description 5"),
+        Note(id: UUID(), title: "Note 6", description: "Description 6"),
+        Note(id: UUID(), title: "Note 7", description: "Description 7"),
+        Note(id: UUID(), title: "Note 8", description: "Description 8"),
+        Note(id: UUID(), title: "Note 9", description: "Description 9"),
+        Note(id: UUID(), title: "Note 10", description: "Description 10"),
+        Note(id: UUID(), title: "Note 11", description: "Description 11"),
+        Note(id: UUID(), title: "Note 12", description: "Description 12")
         
     ]
 }
